659092c9642021acb982152273e2c28f719fb749
Andriy-Lyt <howdy_eh@icloud.com>
2022-01-03 23:04:39 -0500

    complete exs 1-6

--- exercises/exercise_6.rb
+++ exercises/exercise_6.rb
@@ -9,3 +9,13 @@ puts "Exercise 6"
 puts "----------"
 
 # Your code goes here ...
+@store1.employees.create(first_name: "Khurram", last_name: "Virani", hourly_rate: 60)
+
+@store1.employees.create(first_name: "Bahran", last_name: "Hubald", hourly_rate: 50)
+
+@store2.employees.create(first_name: "Mahdir", last_name: "Rahman", hourly_rate: 55)
+
+@store2.employees.create(first_name: "Ashman", last_name: "Abdul", hourly_rate: 49)
+
+@store3.employees.create(first_name: "Durhan", last_name: "Karib", hourly_rate: 45)
+
--- lib/store.rb
+++ lib/store.rb
@@ -1,3 +1,8 @@
 class Store < ActiveRecord::Base
+  has_many :employees
+  belongs_to :store
+
+  # validates :name, presence: true
+  # validates greater_than_or_equal_to: 0
 
 end
